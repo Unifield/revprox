8c896ae803793a550976ae6e77004830f460ce36
jftempo <jfb@tempo-consulting.fr>
2024-05-29 11:22:24 +0200

    Dont use USERPROFILE to store autocert cache

--- revprox.go
+++ revprox.go
@@ -160,9 +160,6 @@ func getCertViaLE(fqdn string) func(*tls.ClientHelloInfo) (*tls.Certificate, err
 func cacheDir() string {
     if runtime.GOOS == "windows" {
         home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
-        if home == "" {
-            home = os.Getenv("USERPROFILE")
-        }
         return filepath.Join(home, "autocert")
     }
     return filepath.Join(os.Getenv("HOME"), ".autocert")
